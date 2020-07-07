a972cdd76f2708e4d175d9c824037ae035417d46
wanggeng <1025964889@qq.com>
2020-07-07 17:44:03 +0800

    绘制三角形

--- OpenglWork/LessonOne/Src/sample.cpp
+++ OpenglWork/LessonOne/Src/sample.cpp
@@ -6,6 +6,20 @@
 #include <iostream>
 
 
+const char  *vertexShaderSource = "#version 330 core\n"
+"layout (location=0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"gl_Position=vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor=vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, 800, 600);
@@ -38,7 +52,7 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 
-
+    
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
@@ -49,12 +63,88 @@ int main()
 
 	}
 
+	//build and compile shader program
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+	glCompileShader(vertexShader);
+	//check error
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::Shader::VertexShader::compilation_failed\n" << infoLog << std::endl;
+	}
+
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::Shader::FragmentShader::compilation_failed\n" << infoLog << std::endl;
+	}
+
+	//link shaders
+
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::shader::program::linking_failed\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,0.5f,0.0f
+	};
+
+	//创建VBO顶点缓冲数组对象
+	unsigned int VBO,VAO;
+
+	glGenVertexArrays(1, &VAO);
+	
+
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	//绑定
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//拷贝
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
